0c7bb6696752666a33275026b2c6efd0e63e5999
Liam Brophy <liambrophy.design@gmail.com>
2025-01-07 18:29:41 -0500

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = (drivers) => drivers.slice(0, 2);
+console.log(returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']));
+
+
+const returnLastTwoDrivers = (drivers) => drivers.slice(2,4); 
+console.log(returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']));
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+const createFareMultiplier = (int) =>  function(multiplier) {
+        return int * multiplier;
+    };
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (arrayOfDrivers, fn) => fn(arrayOfDrivers)
